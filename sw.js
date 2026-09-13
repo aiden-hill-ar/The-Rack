f580853f0c80b68634371c3ce0e610bc2a986d5c
Aiden <aidenhill.ar@gmail.com>
2026-09-12 23:51:00 -0500

    replaced the base tag for removing the starting "/" in the assets array in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'my-pwa-v1';
-const ASSETS = ['/', '/index.html', '/pages/calculator.html', '/pages/tally.html', '/styles/the-rack.css', '/styles/calculator.css', '/styles/tally.css', '/styles/nav.css', '/styles/mini/mini-calculator.css', '/scripts/calculator.js', '/scripts/nav.js', '/scripts/random-color.js', '/svg/the-rack.svg', '/svg/calculator.svg', '/svg/squiggly-line.svg', '/png/the-rack-192.png', '/png/the-rack-512.png', '/manifest.json'];
+const ASSETS = ['.', 'index.html', 'pages/calculator.html', 'pages/tally.html', 'styles/the-rack.css', 'styles/calculator.css', 'styles/tally.css', 'styles/nav.css', 'styles/mini/mini-calculator.css', 'scripts/calculator.js', 'scripts/nav.js', 'scripts/random-color.js', 'svg/the-rack.svg', 'svg/calculator.svg', 'svg/squiggly-line.svg', 'png/the-rack-192.png', 'png/the-rack-512.png', 'manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
