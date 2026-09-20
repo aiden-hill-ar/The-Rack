90edd7a7e7401cacfd490f43fb715156ce1d4ef5
Aiden <aidenhill.ar@gmail.com>
2026-09-19 19:49:33 -0500

    completely disabled all sw stuff (I think)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v1';
+/* const VERSION = 'v1';
 const CACHE_NAME = `The-Rack-${VERSION}`;
 
 const APP_STATIC_RESOURCES = ['./', './index.html', './manifest.json', './pages/calculator.html', './pages/tally.html', './css/the-rack.css', './css/calculator.css', './css/tally.css', './css/mini/mini-calculator.css', './css/calculator.js', './js/random-color.js', './js/calculator.js', './js/tally.js', './svg/squiggly-line.svg', './fonts/Hanuman-Variable.ttf'];
@@ -50,4 +50,4 @@ self.addEventListener("fetch", (event) => {
       return new Response(null, { status: 404 });
     })(),
   );
-});
+}); */
