9d348a401645ddd10990cd0c4db11bc1e53bb9f4
Aiden <aidenhill.ar@gmail.com>
2026-09-19 21:29:49 -0500

    made changes in haptic.js so double tapping fastly won't zoom in

--- js/haptic.js
+++ js/haptic.js
@@ -14,3 +14,12 @@ document.querySelectorAll('.num-button, .operator-button, .special-button')
       navigator.vibrate(15);
     });
   });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function(event) {
+let now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault(); 
+    }
+    lastTouchEnd = now; 
+}, false);
